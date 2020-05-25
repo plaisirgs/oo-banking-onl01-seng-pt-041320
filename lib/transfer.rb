cb31297974e0d2c44a5cf18ad1255d7016e7e585
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-25 16:02:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   
   
   def execute_transaction
-    if @status != "complete"
+    if valid?
     @sender.balance = @sender.balance - @amount
     @receiver.balance = @receiver.balance + @amount
     @status = "complete"

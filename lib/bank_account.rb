9a1cb6de734c5552ed24d3cf6e45550caed58909
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-25 15:19:22 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,8 +3,34 @@ class BankAccount
   attr_accessor :name
   def initialize(name)
     @name = name
+    @balance = 1000
   end
   
  
 end
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# new_account = BankAccount.new("Chase")
+# <....>
+# new_account.name
+# Chase

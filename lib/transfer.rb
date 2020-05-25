34e7f5321f003f012082271265b49b8d57e9752a
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-25 16:03:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,8 @@ class Transfer
     @sender.balance = @sender.balance - @amount
     @receiver.balance = @receiver.balance + @amount
     @status = "complete"
+  else
+    "Transaction rejected. Please check your account balance."
     end
   end
   
